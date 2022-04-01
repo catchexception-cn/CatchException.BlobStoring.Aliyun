cd116008a48949381ba40e8761bc9345c8c79ee4
Chi Zhang <zliming@outlook.com>
2022-04-01 19:26:02 +0800

    添加项目文件。

---
+++ CatchException.BlobStoring.Aliyun/src/AliyunBlobUrlCalculator.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Extensions.Configuration;
+
+using Volo.Abp.DependencyInjection;
+using Volo.Abp.MultiTenancy;
+
+namespace CatchException.BlobStoring.Aliyun;
+
+[Dependency(ReplaceServices = true)]
+[ExposeServices(typeof(IBlobUrlCalculator))]
+public class AliyunBlobUrlCalculator : DefaultBlobUrlCalculator
+{
+    private readonly string _aliyunEndpoint;
+
+    public AliyunBlobUrlCalculator(ICurrentTenant currentTenant,
+        IConfiguration configuration) : base(currentTenant)
+    {
+        _aliyunEndpoint = configuration["BlobStoring:Aliyun:PublicAccessEndpoint"].EnsureEndsWith('/');
+    }
+
+    public override string Calculate(string fileName)
+    {
+        var name = base.Calculate(fileName);
+        return _aliyunEndpoint + name;
+    }
+}
---
+++ CatchException.BlobStoring.Aliyun/src/CatchExceptionBlobStoringAliyunModule.cs
@@ -0,0 +1,11 @@
+﻿using Volo.Abp.BlobStoring.Aliyun;
+using Volo.Abp.Modularity;
+
+namespace CatchException.BlobStoring.Aliyun;
+
+[DependsOn(
+    typeof(AbpBlobStoringAliyunModule))]
+public class CatchExceptionBlobStoringAliyunModule : AbpModule
+{
+
+}
